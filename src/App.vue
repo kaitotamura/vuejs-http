de2a67472a745358297350853fa2571f9c51d6e5
田村 海斗 <kaito.tamura@proptech.plus>
2020-11-09 11:59:43 +0900

    add new file

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div id="app">
+  <h3>掲示板に投稿する</h3>
+  <label for="name">ニックネーム:</label>
+  <input 
+  id="name"
+  type="text"
+  v-model="name"
+  >
+  <br><br>
+  <label for="comment">コメント:</label>
+  <textarea 
+    id="comment"
+    v-model="comment"
+  ></textarea>
+  <br><br>
+  <button @click="createComment">コメントをサーバーに送る</button>
+  <h2>掲示板</h2>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return {
+      name: "",
+      comment: ""
+    };
+  },
+  methods: {
+    createComment() {
+      axios.post();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
